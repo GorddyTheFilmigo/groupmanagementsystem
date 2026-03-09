dd7507270a11d4738993fa49128b0e92e03e53ba
Gordon Odhiambo <godhiambo@kabarak.ac.ke>
2026-03-09 15:48:43 +0300

    continued changes

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE='groupflow-v1';
+const FILES=[
+  '/',
+  '/index.html',
+  '/app.html',
+  '/signup.html'
+];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+});
+
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
+});
