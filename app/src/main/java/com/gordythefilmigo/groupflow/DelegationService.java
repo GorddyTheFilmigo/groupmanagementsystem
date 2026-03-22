a2d03b2c6025bd73d56b800434189c65b70c880e
Gordon Odhiambo <godhiambo@kabarak.ac.ke>
2026-03-22 09:03:01 +0300

    continued changes

---
+++ app/src/main/java/com/gordythefilmigo/groupflow/DelegationService.java
@@ -0,0 +1,14 @@
+package com.gordythefilmigo.groupflow;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
